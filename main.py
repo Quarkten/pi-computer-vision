be0c794fcacb59175fd8d8bab494cba1bafb9dad
Mac_Quarkten <tojoshiparth@gmail.com>
2025-06-24 12:10:14 -0400

    deleted finger detection module

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 from vision.hand_detection import HandDetector
 from vision.face_detection import FaceDetector
-from vision.finger_tracking import FingerTracker
+#from vision.finger_tracking import FingerTracker
 
 cap = cv2.VideoCapture(0)
 detector_hand = HandDetector()
 detector_face = FaceDetector()
-detector_finger = FingerTracker()
+#detector_finger = FingerTracker()
 
 while True:
     ret, frame = cap.read()
@@ -15,7 +15,7 @@ while True:
 
     frame = detector_hand.detect(frame)
     frame = detector_face.detect(frame)
-    frame = detector_finger.track(frame)
+    #frame = detector_finger.track(frame)
 
     cv2.imshow("Pi Vision", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
